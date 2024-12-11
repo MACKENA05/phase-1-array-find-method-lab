19aa700588277af266fc5f7224929b5ec9115b3b
MonicahMackena¨ <mackenamuthui@gmail.com>
2024-12-11 22:17:42 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+//   function superbowlWin(object){
+//       if(object.result === "W"){
+//         return (object.year)
+//   }
+// }
+
+function superbowlWin(record) {
+    const winRecord = record.find(object => object.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+console.log(superbowlWin(record))
